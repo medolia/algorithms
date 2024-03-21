d27c83d7658c6f5f915e522e45ae69e76d430b36
lbli <lbli@trip.com>
2024-03-21 20:24:46 +0800

    224 基本计算器

--- leetcode/others/BasicCalculator.java
+++ leetcode/others/BasicCalculator.java
@@ -4,13 +4,18 @@ import java.util.Stack;
 
 /**
  * <a href="https://leetcode.cn/problems/basic-calculator/description/">224. 基本计算器</a>
+ * <p>
+ * 实际结算应该与输入顺序相反，即使用栈来处理，
+ * <p>数字和符号正常压入栈
+ * <p>当遇到 ”）“ 触发一次括号内结算
  *
  * @author lbli
  */
 public class BasicCalculator {
 
     public static void main(String[] args) {
-        new BasicCalculator().calculate("(1+(4+5+2)-3)+(6+8)");
+//        new BasicCalculator().calculate("-(1+(46+5+2)-3)+(6+8)");
+        int calculate = new BasicCalculator().calculate("2-(5-6)");
 //        new BasicCalculator().calculate("(1+(4+5+2)-3)+(6+8)");
     }
 
@@ -19,55 +24,63 @@ public class BasicCalculator {
      * ): 出栈
      */
     public int calculate(String s) {
-        Stack<Stack<Integer>> mainStack = new Stack<>();
-        mainStack.push(new Stack<>());
+        s = s.replaceAll(" ", "");
+        Stack<Integer> nums = new Stack<>();
+        Stack<Character> ops = new Stack<>();
 
-        int reg = 0;
-        int res = 0;
-        boolean minusFlag = false;
-        for (char c : s.toCharArray()) {
-            if (c == ' ') {
-                continue;
-            } else if (c == '-') {
-                minusFlag = true;
-                continue;
-            }
+        int i = 0;
+        while (i < s.length()) {
+            char c = s.charAt(i);
 
             if (Character.isDigit(c)) {
-                reg = reg * 10 + (c - '0');
-            } else {
-                int validV = minusFlag ? -reg : reg;
-                minusFlag = false;
-                Stack<Integer> curr = mainStack.peek();
-                curr.push(validV);
-                reg = 0;
-            }
-
-            if (c == '+') {
-
-            } else if (c == '(') {
-                Stack<Integer> newStack = new Stack<>();
-                mainStack.push(newStack);
-            } else if (c == ')') {
-                Stack<Integer> finished = mainStack.pop();
-                int finishedRes = 0;
-                while (!finished.isEmpty()) {
-                    finishedRes += finished.pop();
+                int num = 0;
+                while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + (s.charAt(i) - '0');
+                    i++;
                 }
-                mainStack.peek().push(finishedRes);
+                nums.push(num);
+            } else if (c == ' ') {
+                i++;
+                continue;
+            } else if (c == '+' || c == '-' || c == '(') {
+
+                // (-2) -> (0-2)
+                if (c == '-' && i > 0 && s.charAt(i - 1) == '(') {
+                    nums.push(0);
+                }
+                ops.push(c);
+                i++;
+            } else if (c == ')') { // 触发一次结算
+                calc(nums, ops);
+                i++;
             }
-        }
-        mainStack.peek().push(reg);
 
-
-        while (!mainStack.isEmpty()) {
-            Stack<Integer> curr = mainStack.pop();
-            while (!curr.isEmpty()) {
-                res += curr.pop();
-            }
         }
 
-        return res;
+        calc(nums, ops);
+
+        return nums.pop();
     }
 
+    private void calc(Stack<Integer> nums, Stack<Character> ops) {
+        int tempRes = 0;
+        while (!nums.isEmpty() && !ops.isEmpty()) {
+
+
+            int num = nums.pop();
+            Character op = ops.pop();
+
+            int validV = op == '-' ? -num : num;
+            tempRes += validV;
+
+            if (op == '(') {
+                break;
+            }
+        }
+
+        if (nums.size() == 1 && ops.isEmpty()) {
+            tempRes += nums.pop();
+        }
+        nums.push(tempRes);
+    }
 }
