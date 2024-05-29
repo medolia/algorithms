53b956c0dc5ed3d4745ee6ef310a434b3de70003
lbli <lbli@trip.com>
2024-05-29 18:15:33 +0800

    leetcode 23

--- leetcode/arrayAndLinkedList/Leetcode23MergeSortedLinkedList.java
+++ leetcode/arrayAndLinkedList/Leetcode23MergeSortedLinkedList.java
@@ -2,7 +2,6 @@ package arrayAndLinkedList;
 
 import struc.ListNode;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -11,20 +10,37 @@ import java.util.PriorityQueue;
  *
  * @author lbli
  */
-public class Leetcode23MergeSortedLinkedList {
+class Leetcode23MergeSortedLinkedList {
+
+    public static void main(String[] args) {
+        ListNode node1 = ListNode.fromValArr(1, 4, 5);
+        ListNode node2 = ListNode.fromValArr(1, 3, 4);
+        ListNode node3 = ListNode.fromValArr(1, 4, 5);
+
+        ListNode result = new Leetcode23MergeSortedLinkedList().mergeKLists(new ListNode[]{node1, node2, node3});
+        System.out.println(result);
+    }
 
     public ListNode mergeKLists(ListNode[] lists) {
         // 最小堆，堆顶为最小值
-        PriorityQueue<ListNode> priorityQueue = new PriorityQueue<>(Comparator.comparing(e -> e.val));
-        priorityQueue.addAll(Arrays.asList(lists));
+        PriorityQueue<ListNode> heap = new PriorityQueue<>(Comparator.comparing(e -> e.val));
+        for (ListNode node : lists) {
+            ListNode curr = node;
+            while (curr != null) {
+                heap.add(curr);
+
+                curr = curr.next;
+            }
+        }
 
         ListNode dummy = new ListNode(-1);
 
         ListNode curr = dummy;
-        while(!priorityQueue.isEmpty()) {
-            curr.next = priorityQueue.poll();
+        while(!heap.isEmpty()) {
+            curr.next = heap.poll();
             curr = curr.next;
         }
+        curr.next = null;
 
         return dummy.next;
     }
--- leetcode/struc/ListNode.java
+++ leetcode/struc/ListNode.java
@@ -9,4 +9,28 @@ public class ListNode {
     }
 
     public ListNode() { }
+
+    public static ListNode fromValArr(int... arr) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        for (int i : arr) {
+            cur.next = new ListNode(i);
+            cur = cur.next;
+        }
+
+        return dummy.next;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder(String.valueOf(this.val));
+
+        ListNode cur = this.next;
+        while (null != cur) {
+            builder.append("->").append(cur.val);
+            cur = cur.next;
+        }
+
+        return builder.toString();
+    }
 }
